d7f357f7704bd99cff3344cb98e266c16d53df6f
hnanhquoc <hnanhquoc@gmail.com>
2017-02-18 19:18:51 +0700

    add sort

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,13 @@ class HomeController < ApplicationController
   	@sections = Section.all
   	if params[:section_id].present?
   		@current_section = Section.find(params[:section_id])
-  	end
+      @food_items = @current_section.food_items
+    else
+      @food_items = FoodItem.all
+    end
+    if params[:sort_column].present? && params[:sort_direction].present?
+      @food_items = @food_items.order("#{params[:sort_column]} #{params[:sort_direction]}")
+    end
   end
+  
 end
